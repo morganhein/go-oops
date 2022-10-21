29a9ccc05660748c6791e0269ec9ad560a71baf1
Morgan Hein <github@morganhe.in>
2022-10-21 14:38:43 -0500

    ref: renamed injectable to ErrorType

--- oops.go
+++ oops.go
@@ -8,19 +8,19 @@ type OopsI interface {
 	error
 }
 
-type Injectable interface {
+type ErrorType interface {
 	Inject(msg string, err error) OopsI
 }
 
 // New creates a new error of the specified type and returns it
-func New[T Injectable](format string, args ...interface{}) OopsI {
+func New[T ErrorType](format string, args ...interface{}) OopsI {
 	x := *(new(T))
 	msg := fmt.Sprintf(format, args...)
 	x2 := x.Inject(msg, nil)
 	return x2
 }
 
-func Wrap[T Injectable](err error, format string, args ...interface{}) OopsI {
+func Wrap[T ErrorType](err error, format string, args ...interface{}) OopsI {
 	x := *(new(T))
 	msg := fmt.Sprintf(format, args...)
 	x2 := x.Inject(msg, err)
